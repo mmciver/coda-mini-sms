1f1c63c5fa3601e928207011574d47cb598220b0
Michael McIver <mmciver@motologic.com>
2019-10-02 07:22:31 -0700

    typo

--- lib/receiver.rb
+++ lib/receiver.rb
@@ -129,7 +129,7 @@ module CodaMiniSMS
 
       def self.redacted_phone_numbers(status)
         sql = "SELECT * FROM phone_numbers WHERE status = '#{status}'"
-        DB.query(sql)each_with_index.map do |row, i|
+        DB.query(sql).each_with_index.map do |row, i|
           "#{i}: #{row['phone_number'][-4..-1]}"
         end
       end
